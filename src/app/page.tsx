188eb2af3980964cd668814ad1eb2ffa8cad14d1
ejubaal <juan.jesus.albarran.rodriguez@ericsson.com>
2023-10-24 15:29:29 +0200

    #4 Added header and Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
-import StoryList from './components';
+import { Flex } from "@radix-ui/themes";
+
+import Footer from "./components/footer";
+import Header from "./components/header";
+import StoryList from "./components/stories";
+
+
 
 export default async function Home() {
 
   return (
     <main>
-      <StoryList />
+      <Flex direction={'column'} gap={'4'}>
+        <Header/>
+        <StoryList />
+        <Footer />
+      </Flex>
     </main>
   )
 }
